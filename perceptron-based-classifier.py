e87329ab2a48da97b91b0897da9bdabeeab4d3d7
Brian Dao <daobk@vcu.edu>
2023-09-30 19:47:37 -0400

    Added parsing of txt file

--- perceptron-based-classifier.py
+++ perceptron-based-classifier.py
@@ -1,1 +1,15 @@
-import numpy
+import numpy as np
+import pandas as pd
+
+def normalize_data(df):
+    for column in df.columns:
+        df[column] = (df[column] - df[column].min()) / (df[column].max() - df[column].min())
+
+    return df
+
+def parse_data(data):
+    df = pd.read_csv(data, header=None, names = ['Price(USD)', 'Weight(lbs)', 'Type'])
+    return normalize_data(df)
+
+if __name__ == '__main__':
+    print(parse_data('groupA.txt')) 
