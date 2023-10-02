663729fcbc4ee78a7bddfbf75fd648bef04453ff
Brian Dao <daobk@vcu.edu>
2023-10-02 13:22:46 -0400

    debugged the way we adjusted bias

--- perceptron-based-classifier.py
+++ perceptron-based-classifier.py
@@ -7,14 +7,18 @@ def perceptron_based_classifier(df):
     iterations = 5000
     number_of_patterns = 4000
     number_of_inputs = 3
-    alpha = 0.0006
+    alpha = 0.001
     ww = [np.random.uniform(-0.5, 0.5),np.random.uniform(-0.5, 0.5),np.random.uniform(-0.5, 0.5)]
     # change the dataframe to a NumPy array
     patterns = df.to_numpy()
     # since the 3rd column is the label for the type of car, it's our desired output
     dout = np.copy(patterns[:, 2])
+    dout[dout == 0] = -1
+
     # since we saved the dout, change the 3rd column into all 1's so it will always include bias
     patterns[:, 2] = 1
+    
+    
     for iteration in range(0, iterations):
         output = np.zeros(number_of_patterns)
         error_count = 0
@@ -23,17 +27,20 @@ def perceptron_based_classifier(df):
             for i in range(0, number_of_inputs):
                 net = net + ww[i] * patterns[pattern][i]
             # unipolar output
-            output[pattern] = (np.sign(net) + 1) / 2 
+            output[pattern] = np.sign(net)
+
             err = dout[pattern] - output[pattern]
             if err != 0:
-                error_count += 1
+                error_count += 1 
             learn = alpha * err
-            print(iteration, pattern, net, err, learn, ww)
-            for i in range(0, number_of_inputs):
-                ww[i] = ww[i] + learn * patterns[pattern][i]
-        if error_count < 0.0005:
+            #print(iteration, pattern, net, err, learn, ww)
+            for j in range(0, number_of_inputs - 1):
+                ww[j] = ww[j] + learn * patterns[pattern][j]
+            ww[number_of_inputs-1] =  ww[number_of_inputs-1] + learn
+        print(error_count)
+        if error_count < 40:
+            print(ww)
             break
-
       
 def normalize_data(df):
     for column in df.columns:
@@ -46,4 +53,4 @@ def parse_data(data):
     return normalize_data(df)
 
 if __name__ == '__main__':
-    print(perceptron_based_classifier(parse_data('groupA.txt')))
+    print(perceptron_based_classifier(parse_data('groupB.txt')))
